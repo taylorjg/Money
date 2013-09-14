027a8e47e4b49a2ce06188cd1aeee4565111fb96
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-09-14 16:05:44 +0100

    End of chapter 1

---
+++ MoneyExample/Dollar.cs
@@ -0,0 +1,17 @@
+﻿namespace MoneyExample
+{
+    public class Dollar
+    {
+        public int amount;
+
+        public Dollar(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public void Times(int multiplier)
+        {
+            amount *= multiplier;
+        }
+    }
+}
---
+++ MoneyExampleTests/MoneyExampleTests.cs
@@ -0,0 +1,17 @@
+﻿using MoneyExample;
+using NUnit.Framework;
+
+namespace MoneyExampleTests
+{
+    [TestFixture]
+    internal class MoneyExampleTests
+    {
+        [Test]
+        public void TestMultiplication()
+        {
+            var five = new Dollar(5);
+            five.Times(2);
+            Assert.That(five.amount, Is.EqualTo(10));
+        }
+    }
+}
