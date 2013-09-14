59064839f39911fbd44bc24891c619501d3ca221
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-09-14 20:44:29 +0100

    End of chapter 11

--- MoneyExampleTests/MoneyExampleTests.cs
+++ MoneyExampleTests/MoneyExampleTests.cs
@@ -15,14 +15,6 @@ namespace MoneyExampleTests
         }
 
         [Test]
-        public void TestFrancMultiplication()
-        {
-            var five = Money.Franc(5);
-            Assert.That(five.Times(2), Is.EqualTo(Money.Franc(10)));
-            Assert.That(five.Times(3), Is.EqualTo(Money.Franc(15)));
-        }
-
-        [Test]
         public void TestCurrency()
         {
             Assert.That(Money.Dollar(1).Currency, Is.EqualTo("USD"));
@@ -34,15 +26,7 @@ namespace MoneyExampleTests
         {
             Assert.That(Money.Dollar(5), Is.EqualTo(Money.Dollar(5)));
             Assert.That(Money.Dollar(5), Is.Not.EqualTo(Money.Dollar(6)));
-            Assert.That(Money.Franc(5), Is.EqualTo(Money.Franc(5)));
-            Assert.That(Money.Franc(5), Is.Not.EqualTo(Money.Franc(6)));
             Assert.That(Money.Franc(5), Is.Not.EqualTo(Money.Dollar(5)));
-        }
-
-        [Test]
-        public void TestDifferentClassEquality()
-        {
-            Assert.That(new Money(10, "CHF"), Is.EqualTo(new Franc(10, "CHF")));
         }
     }
 }
